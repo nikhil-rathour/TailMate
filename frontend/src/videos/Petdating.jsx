e59e4cbb5a633df3038305c85c9252743a7828bd
nikhil-rathour <rathournikhil042@gmail.com>
2025-07-23 00:24:03 +0530

    dating videio

--- frontend/src/videos/Petdating.jsx
+++ frontend/src/videos/Petdating.jsx
@@ -11,7 +11,7 @@ const PetDatingVideo = () => {
         className="absolute top-0 left-0 w-full h-full object-cover z-0"
       >
         <source
-          src="https://videos.pexels.com/video-files/6503043/6503043-hd_1920_1080_25fps.mp4"
+          src="https://storage.googleapis.com/tailmate_cloud/petdatingfps.mp4"
           type="video/mp4"
         />
         Your browser does not support the video tag.

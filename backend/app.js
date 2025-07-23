7ff8d0a75647ec5311cd895f69e23717fd6e8f8f
nikhil-rathour <rathournikhil042@gmail.com>
2025-07-24 02:22:01 +0530

    deployed problem fixed

--- backend/app.js
+++ backend/app.js
@@ -14,10 +14,7 @@ const authRoutes = require("./routes/auth.router");
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true
-}))
+app.use(cors())
 connectDB()
 
 //routers 

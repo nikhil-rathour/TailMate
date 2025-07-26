96fb9b8065b66309242fc6458bede3f4bf38db35
prathhviraj <soniprithviraj15908@gmail.com>
2025-07-26 09:55:27 +0530

    name update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -141,7 +141,7 @@ export default function Header() {
           >
             {({ isActive }) => (
               <>
-                Bounded by fur
+                Bonded by Fur
                 <span
                   className={`absolute left-1/2 -bottom-1 h-0.5 bg-gold rounded-full transition-all duration-300 transform -translate-x-1/2 ${
                     isActive ? "w-2/3" : "w-0 group-hover:w-2/3"
